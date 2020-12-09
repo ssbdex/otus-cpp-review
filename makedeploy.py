f5bad15c562884cf1fd1537de713b3df0d36d811
Ampermetr123 <sb.simonov@gmail.com>
2020-12-09 16:24:13 +0300

    Revert "remove all for review"
    This reverts commit bb73ad4355ab7c2cd0e86456d11d70f77ecfed69.

---
+++ makedeploy.py
@@ -0,0 +1,35 @@
+import os
+
+text = '''{
+    "package": {
+        "name": "print_ip",
+        "repo": "otus-cpp",
+        "subject": "ampermetr123",
+        "desc": "Homework 3 of OTUS-CPP course",
+        "website_url": "www.jfrog.com",
+        "issue_tracker_url": "https://github.com/Ampermetr123/otus-cpp/issues",
+        "vcs_url": "https://github.com/Ampermetr123/otus-cpp.git",
+        "licenses": ["GPL-2.0"]
+    },
+
+    "version": {
+        "name": "0.0.$TRAVIS_BUILD_NUMBER",
+        "desc": "This is a version"
+    },
+
+    "files":
+        [{"includePattern": "print_ip/build/_CPack_Packages/Linux/DEB/(.*\.deb)", "uploadPattern": "pool/main/p/print_ip/$1",
+            "matrixParams": {
+            "deb_distribution": "trusty",
+            "deb_component": "main",
+            "deb_architecture": "amd64"}
+        }
+        ],
+    "publish": true
+
+}'''
+
+with open("./deploy.txt","w") as f:
+    f.write( text.replace('$TRAVIS_BUILD_NUMBER', os.getenv('TRAVIS_BUILD_NUMBER',0) ))
+
+
